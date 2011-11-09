blamarvt/openstack-integration-tests

Initial import of tests from the Zodiac project. On suggestion from Westmaas, imported tests under the nova directory (final naming TBD) to more quickly get them imported. To run these tests, execute 'nosetests nova/tests'. I've also only submitted the most stable of the tests. More to come.
Change-Id: I2abd961992c02b27c4deaa9f11a49ba91c5b765d

Fixed config defaults

Change-Id: I90d5ea20167caddbec6b4cf51a0df9bb333514cb

utils/data_utils.py
@@ -1,0 +1,4 @@
+import random
+
+def data_gen(self, name='test'):
+    return name + str(random.randint(1, 99999999999))

nova/config.py
@@ -1,0 +1,114 @@
+import ConfigParser
+
+class NovaConfig(object):
+    """Provides configuration information for connecting to Nova."""
+
+    def __init__(self, conf):
+        """Initialize a Nova-specific configuration object."""
+        self.conf = conf
+
+    def get(self, item_name, default_value):
+        try:
+            return self.conf.get("nova", item_name)
+        except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+            return default_value
+
+    @property
+    def auth_url(self):
+        """URL used to authenticate. Defaults to 127.0.0.1."""
+        return self.get("auth_url", "127.0.0.1")
+
+    @property
+    def username(self):
+        """Username to use for Nova API requests. Defaults to 'admin'."""
+        return self.get("user", "admin")
+        
+    @property
+    def tenant_name(self):
+        """Tenant name to use for Nova API requests. Defaults to 'admin'."""
+        return self.get("tenant_name", "admin")
+
+    @property
+    def api_key(self):
+        """API key to use when authenticating. Defaults to 'admin_key'."""
+        return self.get("api_key", "admin_key")
+        
+    @property
+    def build_interval(self):
+        """Time in seconds between build status checks."""
+        return float(self.get("build_interval", 10))
+
+    @property
+    def ssh_timeout(self):
+        """Timeout in seconds to use when connecting via ssh."""
+        return float(self.get("ssh_timeout", 300))
+
+    @property
+    def build_timeout(self):
+        """Timeout in seconds to wait for an entity to build."""
+        return float(self.get("build_timeout", 300))
+
+
+class EnvironmentConfig(object):
+    def __init__(self, conf):
+        """Initialize a Environment-specific configuration object."""
+        self.conf = conf
+
+    def get(self, item_name, default_value):
+        try:
+            return self.conf.get("environment", item_name)
+        except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+            return default_value
+
+    @property
+    def image_ref(self):
+        """Valid imageRef to use """
+        return self.get("image_ref", 3)
+
+    @property
+    def image_ref_alt(self):
+        """Valid imageRef to rebuild images with"""
+        return self.get("image_ref_alt", 3)
+
+    @property
+    def flavor_ref(self):
+        """Valid flavorRef to use"""
+        return int(self.get("flavor_ref", 1))
+
+    @property
+    def flavor_ref_alt(self):
+        """Valid flavorRef to resize images with"""
+        return self.get("flavor_ref_alt", 2)
+
+    @property
+    def resize_available(self):
+        """ Does the test environment support resizing """
+        return self.get("resize_available", 'false') != 'false'
+        
+    @property
+    def create_image_enabled(self):
+        """ Does the test environment support resizing """
+        return self.get("create_image_enabled", 'false') != 'false'
+
+    @property
+    def authentication(self):
+        """ What auth method does the environment use (basic|keystone) """
+        return self.get("authentication", 'keystone')
+
+
+class OpenStackConfig(object):
+    """Provides OpenStack configuration information."""
+
+    _path = "etc/os.config"
+
+    def __init__(self, path=None):
+        """Initialize a configuration from a path."""
+        self._conf = self.load_config(self._path)
+        self.nova = NovaConfig(self._conf)
+        self.env = EnvironmentConfig(self._conf)
+
+    def load_config(self, path=None):
+        """Read configuration from given path and return a config object."""
+        config = ConfigParser.SafeConfigParser()
+        config.read(path)
+        return config

nova/tests/test_flavors.py
@@ -1,0 +1,37 @@
+from nose.plugins.attrib import attr
+from nova import openstack
+import nova.config
+import unittest2 as unittest
+
+class FlavorsTest(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        cls.os = openstack.Manager()
+        cls.client = cls.os.flavors_client
+        cls.config = nova.config.OpenStackConfig()
+        cls.flavor_id = cls.config.env.flavor_ref
+    
+    @attr(type='smoke')    
+    def test_list_flavors(self):
+        """ List of all flavors should contain the expected flavor """
+        resp, body = self.client.list_flavors()
+        flavors = body['flavors']
+        
+        resp, flavor = self.client.get_flavor_details(self.flavor_id)
+        flavor_min_detail = {'id' : flavor['id'], 'links' : flavor['links'], 'name' : flavor['name']}
+        self.assertTrue(flavor_min_detail in flavors)
+    
+    @attr(type='smoke')    
+    def test_list_flavors_with_detail(self):
+        """ Detailed list of all flavors should contain the expected flavor """
+        resp, body = self.client.list_flavors_with_detail()
+        flavors = body['flavors']
+        resp, flavor = self.client.get_flavor_details(self.flavor_id)
+        self.assertTrue(flavor in flavors)
+    
+    @attr(type='smoke')            
+    def test_get_flavor(self):
+        """ The expected flavor details should be returned """
+        resp, flavor = self.client.get_flavor_details(self.flavor_id)
+        self.assertEqual(self.flavor_id, flavor['id'])

nova/tests/test_server_actions.py
@@ -1,0 +1,96 @@
+from nose.plugins.attrib import attr
+from nova import openstack
+import unittest2 as unittest
+import nova.config
+from utils.data_utils import data_gen
+
+class ServerActionsTest(unittest.TestCase):
+    resize_available = nova.config.OpenStackConfig().env.resize_available
+    
+    @classmethod
+    def setUpClass(cls):
+        cls.os = openstack.Manager()
+        cls.client = cls.os.servers_client
+        cls.config = nova.config.OpenStackConfig()
+        cls.image_ref = cls.config.env.image_ref
+        cls.image_ref_alt = cls.config.env.image_ref_alt
+        cls.flavor_ref = cls.config.env.flavor_ref
+        cls.flavor_ref_alt = cls.config.env.flavor_ref_alt
+        
+    def setUp(self):
+        self.name = data_gen('server')
+        resp, server = self.client.create_server(self.name, self.image_ref, 
+                                                 self.flavor_ref)
+        self.id = server['id']
+        self.client.wait_for_server_status(self.id, 'ACTIVE')
+        
+    def tearDown(self):
+        self.client.delete_server(self.id)
+    
+    @attr(type='smoke')   
+    def test_change_server_password(self):
+        """ The server's root password should be set to the provided password """
+        resp, body = self.client.change_password(self.id, 'newpass')
+        self.client.wait_for_server_status(self.id, 'ACTIVE')
+        
+        #TODO: SSH in to verify the new password works
+    
+    @attr(type='smoke')    
+    def test_reboot_server_hard(self):
+        """ The server should be power cycled """
+        #TODO: Add validation the server has been rebooted
+        
+        resp, body = self.client.reboot(self.id, 'HARD')
+        self.client.wait_for_server_status(self.id, 'ACTIVE')
+    
+    @attr(type='smoke')    
+    def test_reboot_server_soft(self):
+        """ The server should be signaled to reboot gracefully """
+        #TODO: Add validation the server has been rebooted
+        
+        resp, body = self.client.reboot(self.id, 'SOFT')
+        self.client.wait_for_server_status(self.id, 'ACTIVE')
+    
+    @attr(type='smoke')     
+    def test_rebuild_server(self):
+        """ The server should be rebuilt using the provided image """
+        
+        self.client.rebuild(self.id, self.image_ref_alt, name='rebuiltserver')
+        self.client.wait_for_server_status(self.id, 'ACTIVE')
+        resp, server = self.client.get_server(self.id)
+        self.assertEqual(self.image_ref_alt, server['image']['id'])
+        self.assertEqual('rebuiltserver', server['name'])
+    
+    @attr(type='smoke')
+    @unittest.skipIf(not resize_available, 'Resize not available.')    
+    def test_resize_server_confirm(self):
+        """ 
+        The server's RAM and disk space should be modified to that of
+        the provided flavor 
+        """
+        
+        self.client.resize(self.id, self.flavor_ref_alt)
+        self.client.wait_for_server_status(self.id, 'VERIFY_RESIZE')
+        
+        self.client.confirm_resize(self.id)
+        self.client.wait_for_server_status(self.id, 'ACTIVE')
+        
+        resp, server = self.client.get_server(self.id)
+        self.assertEqual(self.flavor_ref_alt, server['flavor']['id'])
+    
+    @attr(type='smoke')
+    @unittest.skipIf(not resize_available, 'Resize not available.')     
+    def test_resize_server_revert(self):
+        """ 
+        The server's RAM and disk space should return to its original
+        values after a resize is reverted 
+        """
+        
+        self.client.resize(self.id, self.flavor_ref_alt)
+        self.client.wait_for_server_status(id, 'VERIFY_RESIZE')
+        
+        self.client.revert_resize(self.id)
+        self.client.wait_for_server_status(id, 'ACTIVE')
+        
+        resp, server = self.client.get_server(id)
+        self.assertEqual(self.flavor_ref, server['flavor']['id'])

openstack/tempest

storm/common/utils/data_utils.py
@@ -1,0 +1,5 @@
+import random
+
+
+def rand_name(self, name='test'):
+    return name + str(random.randint(1, 99999999999))

storm/config.py
@@ -1,0 +1,115 @@
+import ConfigParser
+
+
+class NovaConfig(object):
+    """Provides configuration information for connecting to Nova."""
+
+    def __init__(self, conf):
+        """Initialize a Nova-specific configuration object."""
+        self.conf = conf
+
+    def get(self, item_name, default_value):
+        try:
+            return self.conf.get("nova", item_name)
+        except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+            return default_value
+
+    @property
+    def auth_url(self):
+        """URL used to authenticate. Defaults to 127.0.0.1."""
+        return self.get("auth_url", "127.0.0.1")
+
+    @property
+    def username(self):
+        """Username to use for Nova API requests. Defaults to 'admin'."""
+        return self.get("user", "admin")
+
+    @property
+    def tenant_name(self):
+        """Tenant name to use for Nova API requests. Defaults to 'admin'."""
+        return self.get("tenant_name", "admin")
+
+    @property
+    def api_key(self):
+        """API key to use when authenticating. Defaults to 'admin_key'."""
+        return self.get("api_key", "admin_key")
+
+    @property
+    def build_interval(self):
+        """Time in seconds between build status checks."""
+        return float(self.get("build_interval", 10))
+
+    @property
+    def ssh_timeout(self):
+        """Timeout in seconds to use when connecting via ssh."""
+        return float(self.get("ssh_timeout", 300))
+
+    @property
+    def build_timeout(self):
+        """Timeout in seconds to wait for an entity to build."""
+        return float(self.get("build_timeout", 300))
+
+
+class EnvironmentConfig(object):
+    def __init__(self, conf):
+        """Initialize a Environment-specific configuration object."""
+        self.conf = conf
+
+    def get(self, item_name, default_value):
+        try:
+            return self.conf.get("environment", item_name)
+        except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+            return default_value
+
+    @property
+    def image_ref(self):
+        """Valid imageRef to use """
+        return self.get("image_ref", 3)
+
+    @property
+    def image_ref_alt(self):
+        """Valid imageRef to rebuild images with"""
+        return self.get("image_ref_alt", 3)
+
+    @property
+    def flavor_ref(self):
+        """Valid flavorRef to use"""
+        return int(self.get("flavor_ref", 1))
+
+    @property
+    def flavor_ref_alt(self):
+        """Valid flavorRef to resize images with"""
+        return self.get("flavor_ref_alt", 2)
+
+    @property
+    def resize_available(self):
+        """ Does the test environment support resizing """
+        return self.get("resize_available", 'false') != 'false'
+
+    @property
+    def create_image_enabled(self):
+        """ Does the test environment support resizing """
+        return self.get("create_image_enabled", 'false') != 'false'
+
+    @property
+    def authentication(self):
+        """ What auth method does the environment use (basic|keystone) """
+        return self.get("authentication", 'keystone')
+
+
+class StormConfig(object):
+    """Provides OpenStack configuration information."""
+
+    _path = "etc/storm.conf"
+
+    def __init__(self, path=None):
+        """Initialize a configuration from a path."""
+        self._conf = self.load_config(self._path)
+        self.nova = NovaConfig(self._conf)
+        self.env = EnvironmentConfig(self._conf)
+
+    def load_config(self, path=None):
+        """Read configuration from given path and return a config object."""
+        config = ConfigParser.SafeConfigParser()
+        config.read(path)
+        return config

storm/tests/test_flavors.py
@@ -1,0 +1,39 @@
+from nose.plugins.attrib import attr
+from storm import openstack
+import storm.config
+import unittest2 as unittest
+
+
+class FlavorsTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.os = openstack.Manager()
+        cls.client = cls.os.flavors_client
+        cls.config = storm.config.StormConfig()
+        cls.flavor_id = cls.config.env.flavor_ref
+
+    @attr(type='smoke')
+    def test_list_flavors(self):
+        """ List of all flavors should contain the expected flavor """
+        resp, body = self.client.list_flavors()
+        flavors = body['flavors']
+
+        resp, flavor = self.client.get_flavor_details(self.flavor_id)
+        flavor_min_detail = {'id': flavor['id'], 'links': flavor['links'],
+                             'name': flavor['name']}
+        self.assertTrue(flavor_min_detail in flavors)
+
+    @attr(type='smoke')
+    def test_list_flavors_with_detail(self):
+        """ Detailed list of all flavors should contain the expected flavor """
+        resp, body = self.client.list_flavors_with_detail()
+        flavors = body['flavors']
+        resp, flavor = self.client.get_flavor_details(self.flavor_id)
+        self.assertTrue(flavor in flavors)
+
+    @attr(type='smoke')
+    def test_get_flavor(self):
+        """ The expected flavor details should be returned """
+        resp, flavor = self.client.get_flavor_details(self.flavor_id)
+        self.assertEqual(self.flavor_id, flavor['id'])

storm/tests/test_server_actions.py
@@ -1,0 +1,96 @@
+from nose.plugins.attrib import attr
+from storm import openstack
+import unittest2 as unittest
+import storm.config
+from storm.common.utils.data_utils import rand_name
+
+
+class ServerActionsTest(unittest.TestCase):
+    resize_available = storm.config.StormConfig().env.resize_available
+
+    @classmethod
+    def setUpClass(cls):
+        cls.os = openstack.Manager()
+        cls.client = cls.os.servers_client
+        cls.config = storm.config.StormConfig()
+        cls.image_ref = cls.config.env.image_ref
+        cls.image_ref_alt = cls.config.env.image_ref_alt
+        cls.flavor_ref = cls.config.env.flavor_ref
+        cls.flavor_ref_alt = cls.config.env.flavor_ref_alt
+
+    def setUp(self):
+        self.name = rand_name('server')
+        resp, server = self.client.create_server(self.name, self.image_ref,
+                                                 self.flavor_ref)
+        self.id = server['id']
+        self.client.wait_for_server_status(self.id, 'ACTIVE')
+
+    def tearDown(self):
+        self.client.delete_server(self.id)
+
+    @attr(type='smoke')
+    def test_change_server_password(self):
+        """ The server's password should be set to the provided password """
+        resp, body = self.client.change_password(self.id, 'newpass')
+        self.client.wait_for_server_status(self.id, 'ACTIVE')
+        #TODO: SSH in to verify the new password works
+
+    @attr(type='smoke')
+    def test_reboot_server_hard(self):
+        """ The server should be power cycled """
+        #TODO: Add validation the server has been rebooted
+
+        resp, body = self.client.reboot(self.id, 'HARD')
+        self.client.wait_for_server_status(self.id, 'ACTIVE')
+
+    @attr(type='smoke')
+    def test_reboot_server_soft(self):
+        """ The server should be signaled to reboot gracefully """
+        #TODO: Add validation the server has been rebooted
+
+        resp, body = self.client.reboot(self.id, 'SOFT')
+        self.client.wait_for_server_status(self.id, 'ACTIVE')
+
+    @attr(type='smoke')
+    def test_rebuild_server(self):
+        """ The server should be rebuilt using the provided image """
+
+        self.client.rebuild(self.id, self.image_ref_alt, name='rebuiltserver')
+        self.client.wait_for_server_status(self.id, 'ACTIVE')
+        resp, server = self.client.get_server(self.id)
+        self.assertEqual(self.image_ref_alt, server['image']['id'])
+        self.assertEqual('rebuiltserver', server['name'])
+
+    @attr(type='smoke')
+    @unittest.skipIf(not resize_available, 'Resize not available.')
+    def test_resize_server_confirm(self):
+        """
+        The server's RAM and disk space should be modified to that of
+        the provided flavor
+        """
+
+        self.client.resize(self.id, self.flavor_ref_alt)
+        self.client.wait_for_server_status(self.id, 'VERIFY_RESIZE')
+
+        self.client.confirm_resize(self.id)
+        self.client.wait_for_server_status(self.id, 'ACTIVE')
+
+        resp, server = self.client.get_server(self.id)
+        self.assertEqual(self.flavor_ref_alt, server['flavor']['id'])
+
+    @attr(type='smoke')
+    @unittest.skipIf(not resize_available, 'Resize not available.')
+    def test_resize_server_revert(self):
+        """
+        The server's RAM and disk space should return to its original
+        values after a resize is reverted
+        """
+
+        self.client.resize(self.id, self.flavor_ref_alt)
+        self.client.wait_for_server_status(id, 'VERIFY_RESIZE')
+
+        self.client.revert_resize(self.id)
+        self.client.wait_for_server_status(id, 'ACTIVE')
+
+        resp, server = self.client.get_server(id)
+        self.assertEqual(self.flavor_ref, server['flavor']['id'])