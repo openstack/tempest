e81028a3f08cf4e50675f9b37dfb7519384d7094
saradpatel <sarad.patel@hp.com>
2012-05-02 22:36:06 -0700

    Fixed the LP bug 993739.
    Ensures that the servers created in the tests are destroyed in finally: block in the test.
    
    Change-Id: If5fb27f7dd7f99293ade59a23dd1aa36d450f850

--- tempest/tests/test_servers.py
+++ tempest/tests/test_servers.py
@@ -54,58 +54,65 @@ class ServersTest(BaseComputeTest):
         password should be set to that password.
         """
 
-        name = rand_name('server')
-        resp, server = self.client.create_server(name, self.image_ref,
+        try:
+            name = rand_name('server')
+            resp, server = self.client.create_server(name, self.image_ref,
                                                  self.flavor_ref,
                                                  adminPass='testpassword')
 
-        #Verify the password is set correctly in the response
-        self.assertEqual('testpassword', server['adminPass'])
+            #Verify the password is set correctly in the response
+            self.assertEqual('testpassword', server['adminPass'])
 
         #Teardown
-        self.client.delete_server(server['id'])
+        finally:
+            self.client.delete_server(server['id'])
 
     @attr(type='smoke')
     def test_update_server_name(self):
         """The server name should be changed to the the provided value"""
-        name = rand_name('server')
-        resp, server = self.client.create_server(name, self.image_ref,
+        try:
+            name = rand_name('server')
+            resp, server = self.client.create_server(name, self.image_ref,
                                                  self.flavor_ref)
-        self.client.wait_for_server_status(server['id'], 'ACTIVE')
+            self.client.wait_for_server_status(server['id'], 'ACTIVE')
 
-        #Update the server with a new name
-        resp, server = self.client.update_server(server['id'], name='newname')
-        self.assertEquals(200, resp.status)
-        self.client.wait_for_server_status(server['id'], 'ACTIVE')
+            #Update the server with a new name
+            resp, server = self.client.update_server(server['id'],
+                                                 name='newname')
+            self.assertEquals(200, resp.status)
+            self.client.wait_for_server_status(server['id'], 'ACTIVE')
 
-        #Verify the name of the server has changed
-        resp, server = self.client.get_server(server['id'])
-        self.assertEqual('newname', server['name'])
+            #Verify the name of the server has changed
+            resp, server = self.client.get_server(server['id'])
+            self.assertEqual('newname', server['name'])
 
         #Teardown
-        self.client.delete_server(server['id'])
+        finally:
+            self.client.delete_server(server['id'])
 
     @attr(type='smoke')
     def test_update_access_server_address(self):
         """
         The server's access addresses should reflect the provided values
         """
-        name = rand_name('server')
-        resp, server = self.client.create_server(name, self.image_ref,
+        try:
+            name = rand_name('server')
+            resp, server = self.client.create_server(name, self.image_ref,
                                                  self.flavor_ref)
-        self.client.wait_for_server_status(server['id'], 'ACTIVE')
+            self.client.wait_for_server_status(server['id'], 'ACTIVE')
 
-        #Update the IPv4 and IPv6 access addresses
-        resp, body = self.client.update_server(server['id'],
+            #Update the IPv4 and IPv6 access addresses
+            resp, body = self.client.update_server(server['id'],
                                                accessIPv4='1.1.1.1',
                                                accessIPv6='::babe:2.2.2.2')
-        self.assertEqual(200, resp.status)
-        self.client.wait_for_server_status(server['id'], 'ACTIVE')
+            self.assertEqual(200, resp.status)
+            self.client.wait_for_server_status(server['id'], 'ACTIVE')
 
-        #Verify the access addresses have been updated
-        resp, server = self.client.get_server(server['id'])
-        self.assertEqual('1.1.1.1', server['accessIPv4'])
-        self.assertEqual('::babe:2.2.2.2', server['accessIPv6'])
+            #Verify the access addresses have been updated
+            resp, server = self.client.get_server(server['id'])
+            self.assertEqual('1.1.1.1', server['accessIPv4'])
+            self.assertEqual('::babe:2.2.2.2', server['accessIPv6'])
 
         #Teardown
-        self.client.delete_server(server['id'])
+        finally:
+            self.client.delete_server(server['id'])
