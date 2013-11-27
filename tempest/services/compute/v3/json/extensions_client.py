78e28bd45c2821f3b1338d793aa637ad86fc163d
Matthew Treinish <treinish@linux.vnet.ibm.com>
2013-11-27 15:58:58 +0000

    Improve testing of list_extensions for compute
    This commit takes the testing for list_extension from the volumes
    extensions client and uses it for the compute testing of
    list_extensions. This also fixes a skip decorator to use use
    requires_ext instead of calling is_extension_enabled directly.
    
    Change-Id: Id08f39e09df0aea0d3c8f507f421c82ddf32900f

--- tempest/services/compute/v3/json/extensions_client.py
+++ tempest/services/compute/v3/json/extensions_client.py
@@ -32,7 +32,7 @@ class ExtensionsV3ClientJSON(RestClient):
         url = 'extensions'
         resp, body = self.get(url)
         body = json.loads(body)
-        return resp, body
+        return resp, body['extensions']
 
     def is_enabled(self, extension):
         _, extensions = self.list_extensions()
