1461c786f1cb9b0b3cd37f5d11b76151e8726bf5
Haiwei Xu <xu-haiwei@mxw.nes.nec.co.jp>
2013-12-09 10:14:13 +0900

    Separate negative tests for test_services in Tempest
    Move negative tests from test_services to test_services_negatives.py
    
    Change-Id: I4fe49133e005f64dc17ea6f484ffa206aba274e8

--- tempest/api/compute/admin/test_services_negative.py
+++ tempest/api/compute/admin/test_services_negative.py
@@ -1,8 +1,6 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
-
-# Copyright 2013 NEC Corporation
-# Copyright 2013 IBM Corp.
-# All Rights Reserved.
+#
+# Copyright 2013 NEC Corporation.  All rights reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -17,6 +15,7 @@
 #    under the License.
 
 from tempest.api.compute import base
+from tempest import exceptions
 from tempest.test import attr
 
 
@@ -34,51 +33,37 @@ class ServicesAdminTestJSON(base.BaseV2ComputeAdminTest):
         cls.client = cls.os_adm.services_client
         cls.non_admin_client = cls.services_client
 
-    @attr(type='gate')
-    def test_list_services(self):
+    @attr(type=['negative', 'gate'])
+    def test_list_services_with_non_admin_user(self):
+        self.assertRaises(exceptions.Unauthorized,
+                          self.non_admin_client.list_services)
+
+    @attr(type=['negative', 'gate'])
+    def test_get_service_by_invalid_params(self):
+        # return all services if send the request with invalid parameter
         resp, services = self.client.list_services()
+        params = {'xxx': 'nova-compute'}
+        resp, services_xxx = self.client.list_services(params)
         self.assertEqual(200, resp.status)
-        self.assertNotEqual(0, len(services))
+        self.assertEqual(len(services), len(services_xxx))
 
-    @attr(type='gate')
-    def test_get_service_by_service_binary_name(self):
-        binary_name = 'nova-compute'
-        params = {'binary': binary_name}
-        resp, services = self.client.list_services(params)
-        self.assertEqual(200, resp.status)
-        self.assertNotEqual(0, len(services))
-        for service in services:
-            self.assertEqual(binary_name, service['binary'])
-
-    @attr(type='gate')
-    def test_get_service_by_host_name(self):
+    @attr(type=['negative', 'gate'])
+    def test_get_service_by_invalid_service_and_valid_host(self):
         resp, services = self.client.list_services()
         host_name = services[0]['host']
-        services_on_host = [service for service in services if
-                            service['host'] == host_name]
-        params = {'host': host_name}
+        params = {'host': host_name, 'binary': 'xxx'}
         resp, services = self.client.list_services(params)
+        self.assertEqual(200, resp.status)
+        self.assertEqual(0, len(services))
 
-        # we could have a periodic job checkin between the 2 service
-        # lookups, so only compare binary lists.
-        s1 = map(lambda x: x['binary'], services)
-        s2 = map(lambda x: x['binary'], services_on_host)
-
-        # sort the lists before comparing, to take out dependency
-        # on order.
-        self.assertEqual(sorted(s1), sorted(s2))
-
-    @attr(type='gate')
-    def test_get_service_by_service_and_host_name(self):
+    @attr(type=['negative', 'gate'])
+    def test_get_service_with_valid_service_and_invalid_host(self):
         resp, services = self.client.list_services()
-        host_name = services[0]['host']
         binary_name = services[0]['binary']
-        params = {'host': host_name, 'binary': binary_name}
+        params = {'host': 'xxx', 'binary': binary_name}
         resp, services = self.client.list_services(params)
         self.assertEqual(200, resp.status)
-        self.assertEqual(1, len(services))
-        self.assertEqual(host_name, services[0]['host'])
-        self.assertEqual(binary_name, services[0]['binary'])
+        self.assertEqual(0, len(services))
 
 
 class ServicesAdminTestXML(ServicesAdminTestJSON):
