e8136f08baf7e3afd9523678539f616317cb84f9
Zhi Kun Liu <zhikunli@cn.ibm.com>
2014-01-07 18:56:28 +0800

    add keystone group tests
    add two testcases for group and group users:
    test_group_create_update_get
    test_group_users_add_list_delete
    
    Change-Id: I9fa1821e219c54715f821999dac100daa68283f0

---
+++ tempest/api/identity/admin/v3/test_groups.py
@@ -0,0 +1,84 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2013 IBM Corp.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from tempest.api.identity import base
+from tempest.common.utils import data_utils
+from tempest import test
+
+
+class GroupsV3TestJSON(base.BaseIdentityAdminTest):
+    _interface = 'json'
+
+    @classmethod
+    def setUpClass(cls):
+        super(GroupsV3TestJSON, cls).setUpClass()
+
+    @test.attr(type='smoke')
+    def test_group_create_update_get(self):
+        name = data_utils.rand_name('Group')
+        description = data_utils.rand_name('Description')
+        resp, group = self.v3_client.create_group(name,
+                                                  description=description)
+        self.addCleanup(self.v3_client.delete_group, group['id'])
+        self.assertEqual(resp['status'], '201')
+        self.assertEqual(group['name'], name)
+        self.assertEqual(group['description'], description)
+
+        new_name = data_utils.rand_name('UpdateGroup')
+        new_desc = data_utils.rand_name('UpdateDescription')
+        resp, updated_group = self.v3_client.update_group(group['id'],
+                                                          name=new_name,
+                                                          description=new_desc)
+        self.assertEqual(resp['status'], '200')
+        self.assertEqual(updated_group['name'], new_name)
+        self.assertEqual(updated_group['description'], new_desc)
+
+        resp, new_group = self.v3_client.get_group(group['id'])
+        self.assertEqual(resp['status'], '200')
+        self.assertEqual(group['id'], new_group['id'])
+        self.assertEqual(new_name, new_group['name'])
+        self.assertEqual(new_desc, new_group['description'])
+
+    @test.attr(type='smoke')
+    def test_group_users_add_list_delete(self):
+        name = data_utils.rand_name('Group')
+        resp, group = self.v3_client.create_group(name)
+        self.addCleanup(self.v3_client.delete_group, group['id'])
+        # add user into group
+        users = []
+        for i in range(3):
+            name = data_utils.rand_name('User')
+            resp, user = self.v3_client.create_user(name)
+            users.append(user)
+            self.addCleanup(self.v3_client.delete_user, user['id'])
+            self.v3_client.add_group_user(group['id'], user['id'])
+
+        # list users in group
+        resp, group_users = self.v3_client.list_group_users(group['id'])
+        self.assertEqual(resp['status'], '200')
+        self.assertEqual(users.sort(), group_users.sort())
+        # delete user in group
+        for user in users:
+            resp, body = self.v3_client.delete_group_user(group['id'],
+                                                          user['id'])
+            self.assertEqual(resp['status'], '204')
+        resp, group_users = self.v3_client.list_group_users(group['id'])
+        self.assertEqual(len(group_users), 0)
+
+
+class GroupsV3TestXML(GroupsV3TestJSON):
+    _interface = 'xml'
