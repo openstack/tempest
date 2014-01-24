e699f82050be6e3b50f7d814f2a8f60ba0832876
Matt Riedemann <mriedem@us.ibm.com>
2014-01-24 08:11:42 -0800

    Add more descriptive assertion message for test_create_backup
    In order to fingerprint the failure for gate race bug 1267326 in
    elastic-recheck, the assertion message needs to be specific to the test
    case that's failing. We have to do something like this until we have
    multi-line query support in elastic-recheck.
    
    Also makes the backup names more specific.
    
    Related-Bug: #1267326
    
    Change-Id: I4cf4a3409eedf64f928ae7b0ff4a660c116b89bc

--- tempest/api/compute/servers/test_server_actions.py
+++ tempest/api/compute/servers/test_server_actions.py
@@ -230,7 +230,7 @@ class ServerActionsTestJSON(base.BaseV2ComputeTest):
     def test_create_backup(self):
         # Positive test:create backup successfully and rotate backups correctly
         # create the first and the second backup
-        backup1 = data_utils.rand_name('backup')
+        backup1 = data_utils.rand_name('backup-1')
         resp, _ = self.servers_client.create_backup(self.server_id,
                                                     'daily',
                                                     2,
@@ -247,7 +247,7 @@ class ServerActionsTestJSON(base.BaseV2ComputeTest):
         self.assertEqual(202, resp.status)
         self.os.image_client.wait_for_image_status(image1_id, 'active')
 
-        backup2 = data_utils.rand_name('backup')
+        backup2 = data_utils.rand_name('backup-2')
         self.servers_client.wait_for_server_status(self.server_id, 'ACTIVE')
         resp, _ = self.servers_client.create_backup(self.server_id,
                                                     'daily',
@@ -275,7 +275,7 @@ class ServerActionsTestJSON(base.BaseV2ComputeTest):
 
         # create the third one, due to the rotation is 2,
         # the first one will be deleted
-        backup3 = data_utils.rand_name('backup')
+        backup3 = data_utils.rand_name('backup-3')
         self.servers_client.wait_for_server_status(self.server_id, 'ACTIVE')
         resp, _ = self.servers_client.create_backup(self.server_id,
                                                     'daily',
@@ -293,7 +293,11 @@ class ServerActionsTestJSON(base.BaseV2ComputeTest):
             sort_key='created_at',
             sort_dir='asc')
         self.assertEqual(200, resp.status)
-        self.assertEqual(2, len(image_list))
+        self.assertEqual(2, len(image_list),
+                         'Unexpected number of images for '
+                         'v2:test_create_backup; was the oldest backup not '
+                         'yet deleted? Image list: %s' %
+                         [image['name'] for image in image_list])
         self.assertEqual((backup2, backup3),
                          (image_list[0]['name'], image_list[1]['name']))
 
