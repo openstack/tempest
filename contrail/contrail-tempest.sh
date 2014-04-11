0fb76d75f6dd172d7094eb28cdf5539a6e504ab5
Vedu Joshi <vjoshi@juniper.net>
2014-04-11 13:33:49 +0530

    Initial changes towards a working setup

--- contrail/contrail-tempest.sh
+++ contrail/contrail-tempest.sh
@@ -1,11 +1,13 @@
 apt-get install -y git
+apt-get install -y libffi-dev
+apt-get install -y gcc
 if [ ! -d "tempest" ]; then
    git clone git://github.com/vedujoshi/tempest
 else
    (cd tempest ; git pull)
 fi
   
-(cd tempest;git checkout remotes/origin/stable/havana)
+#(cd tempest;git checkout remotes/origin/stable/havana)
 
 DEST="$PWD/tempest"
 DATA_DIR=${DEST}/data
@@ -21,13 +23,21 @@ TEMPEST_CONFIG_DIR=${TEMPEST_CONFIG_DIR:-$TEMPEST_DIR/etc}
 TEMPEST_CONFIG=$TEMPEST_CONFIG_DIR/tempest.conf
 TEMPEST_STATE_PATH=${TEMPEST_STATE_PATH:=$DATA_DIR/tempest}
 
-source $TEMPEST_DIR/tempest/functions
-source /etc/contrail/openstackrc
+source $TEMPEST_DIR/contrail/functions
+#source /etc/contrail/openstackrc
+source ~/openstackrc
 
 cp $TEMPEST_DIR/etc/tempest.conf.sample $TEMPEST_CONFIG
 password=${ADMIN_PASSWORD:-contrail123}
 ALT_USERNAME=${ALT_USERNAME:-alt_demo}
 ALT_TENANT_NAME=${ALT_TENANT_NAME:-alt_demo}
+USERNAME="demo"
+PASSWORD="contrail123"
+TENANT_NAME="demo"
+
+ADMIN_USERNAME="admin"
+ADMIN_PASSWORD="contrail123"
+ADMIN_TENANT_NAME="admin"
 
 # ADD GLANCE IMAGE 
 # set image_uuid and image_uuid_alt
@@ -49,17 +59,22 @@ iniset $TEMPEST_CONFIG DEFAULT debug True
 #IDENTITY
 iniset $TEMPEST_CONFIG identity uri "$KEYSTONE_SERVICE_PROTOCOL://$KEYSTONE_SERVICE_HOST:5000/v2.0/"
 iniset $TEMPEST_CONFIG identity uri_v3 "$KEYSTONE_SERVICE_PROTOCOL://$KEYSTONE_SERVICE_HOST:5000/v3/"
-iniset $TEMPEST_CONFIG identity password "$password"
-iniset $TEMPEST_CONFIG identity admin_password "$password"
+iniset $TEMPEST_CONFIG identity username $USERNAME
+iniset $TEMPEST_CONFIG identity password $PASSWORD
+iniset $TEMPEST_CONFIG identity tenant_name $TENANT_NAME
+
 iniset $TEMPEST_CONFIG identity alt_username $ALT_USERNAME
-iniset $TEMPEST_CONFIG identity alt_password "$password"
+iniset $TEMPEST_CONFIG identity alt_password $password
 iniset $TEMPEST_CONFIG identity alt_tenant_name $ALT_TENANT_NAME
-iniset $TEMPEST_CONFIG identity admin_password "$password"
+
+iniset $TEMPEST_CONFIG identity admin_username $ADMIN_USERNAME
+iniset $TEMPEST_CONFIG identity admin_password $ADMIN_PASSWORD
+iniset $TEMPEST_CONFIG identity admin_tenant_name $ADMIN_TENANT_NAME
 
 iniset $TEMPEST_CONFIG image http_image $HTTP_IMAGE_PATH
 
 #COMPUTE 
-public_network_id=$(source /etc/contrail/openstackrc; neutron net-list | grep $PUBLIC_NETWORK_NAME | \
+public_network_id=$(source ~/openstackrc; neutron net-list | grep $PUBLIC_NETWORK_NAME | \
             awk '{print $2}')
 
 iniset $TEMPEST_CONFIG compute ssh_user ${DEFAULT_INSTANCE_USER:-cirros}
