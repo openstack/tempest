88f7b70da16174c258d172d0b432a30a8a93c8b0
armando-migliaccio <armamig@gmail.com>
2014-06-05 12:59:09 -0700

    Add API tests for Neutron DVR extension
    This patch adds a number of positive and negative tests for
    the DVR functionality implemented by Neutron.
    
    Change-Id: I6d3fbf7c480c23a4500af115891f63ea058dcfac

--- tempest/api/network/test_routers_negative.py
+++ tempest/api/network/test_routers_negative.py
@@ -114,3 +114,28 @@ class RoutersNegativeTest(base.BaseRouterTest):
 
 class RoutersNegativeIpV6Test(RoutersNegativeTest):
     _ip_version = 6
+
+
+class DvrRoutersNegativeTest(base.BaseRouterTest):
+
+    @classmethod
+    def skip_checks(cls):
+        super(DvrRoutersNegativeTest, cls).skip_checks()
+        if not test.is_extension_enabled('dvr', 'network'):
+            msg = "DVR extension not enabled."
+            raise cls.skipException(msg)
+
+    @classmethod
+    def resource_setup(cls):
+        super(DvrRoutersNegativeTest, cls).resource_setup()
+        cls.router = cls.create_router(data_utils.rand_name('router'))
+        cls.network = cls.create_network()
+        cls.subnet = cls.create_subnet(cls.network)
+
+    @test.attr(type=['negative', 'smoke'])
+    @test.idempotent_id('4990b055-8fc7-48ab-bba7-aa28beaad0b9')
+    def test_router_create_tenant_distributed_returns_forbidden(self):
+        self.assertRaises(lib_exc.Forbidden,
+                          self.create_router,
+                          data_utils.rand_name('router'),
+                          distributed=True)
