2a86f1c6d7c09cc668e188b95474a0cbfbd60a12
Adam Gandelman <adamg@ubuntu.com>
2014-06-18 11:34:42 -0700

    Move baremetal API tests to an /admin subdir
    To avoid confusion, move all baremetal API tests to api/baremetal/admin and
    add a README.rst explaining that the API provided by Ironic is meant to be used
    as an admin and service level API.
    
    Change-Id: Ib49b6d5937da6eedc3028a1f831552cf02d9f69e

--- tempest/api/baremetal/admin/test_ports_negative.py
+++ tempest/api/baremetal/admin/test_ports_negative.py
@@ -10,7 +10,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from tempest.api.baremetal import base
+from tempest.api.baremetal.admin import base
 from tempest.common.utils import data_utils
 from tempest import exceptions as exc
 from tempest import test
