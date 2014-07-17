2675f8f561d73374a6e215de4f05d6377f9c8903
Masayuki Igawa <igawa@mxs.nes.nec.co.jp>
2014-07-17 13:46:26 +0200

    Migrate test_dashboard_basic_ops to tempest clients
    Just switched from OfficialClientTest to ScenarioTest because
    TestDashboardBasicOps does not use methods of OfficialClientTest.
    
    Change-Id: I3b8eb90e058cde03cb40f120bb2c268f31ae078c
    Partially-implements: bp:tempest-client-scenarios

--- tempest/scenario/test_dashboard_basic_ops.py
+++ tempest/scenario/test_dashboard_basic_ops.py
@@ -24,7 +24,7 @@ from tempest import test
 CONF = config.CONF
 
 
-class TestDashboardBasicOps(manager.OfficialClientTest):
+class TestDashboardBasicOps(manager.ScenarioTest):
 
     """
     This is a basic scenario test:
