b810c1efc3c3e50ac54b86a5838d44b9a36493ff
Kirill Shileev <kshileev@gmail.com>
2014-08-05 19:11:35 +0400

    adopt ip_version settings for scenario tests

--- tempest/scenario/manager.py
+++ tempest/scenario/manager.py
@@ -914,6 +914,7 @@ class OfficialClientTest(tempest.test.BaseTestCase):
     @classmethod
     def setUpClass(cls):
         super(OfficialClientTest, cls).setUpClass()
+        cls.network_resources.setdefault('ip_version', cls._ip_version)
         cls.isolated_creds = isolated_creds.IsolatedCreds(
             cls.__name__, tempest_client=False,
             network_resources=cls.network_resources)
@@ -1632,6 +1633,7 @@ class NetworkScenarioTest(OfficialClientTest):
     """
     Base class for network scenario tests
     """
+    _ip_version = 4
 
     @classmethod
     def check_preconditions(cls):
@@ -1710,12 +1712,16 @@ class NetworkScenarioTest(OfficialClientTest):
             cidr_in_use = self._list_subnets(tenant_id=tenant_id, cidr=cidr)
             return len(cidr_in_use) != 0
 
-        tenant_cidr = netaddr.IPNetwork(CONF.network.tenant_network_cidr)
+        if self._ip_version == 6:
+            tenant_cidr = netaddr.IPNetwork(CONF.network.tenant_network_v6_cidr)
+            network_prefix = CONF.network.tenant_network_v6_mask_bits
+        else:
+            tenant_cidr = netaddr.IPNetwork(CONF.network.tenant_network_cidr)
+            network_prefix = CONF.network.tenant_network_mask_bits
         result = None
         # Repeatedly attempt subnet creation with sequential cidr
         # blocks until an unallocated block is found.
-        for subnet_cidr in tenant_cidr.subnet(
-            CONF.network.tenant_network_mask_bits):
+        for subnet_cidr in tenant_cidr.subnet(network_prefix):
             str_cidr = str(subnet_cidr)
             if cidr_in_use(str_cidr, tenant_id=network.tenant_id):
                 continue
@@ -1723,7 +1729,7 @@ class NetworkScenarioTest(OfficialClientTest):
             body = dict(
                 subnet=dict(
                     name=data_utils.rand_name(namestart),
-                    ip_version=4,
+                    ip_version=self._ip_version,
                     network_id=network.id,
                     tenant_id=network.tenant_id,
                     cidr=str_cidr,
