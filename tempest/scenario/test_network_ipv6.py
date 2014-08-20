c2944b058f855ccbe8a5f7399c9fb141c1d0fdc5
Kirill Shileev <kshileev@cisco.com>
2014-08-20 19:27:46 +0400

    2 radvd specific tests with a number of utilities: radvd to start RA daemon on specified interface sniffer to collect icmp6 packets on specified interface threaded version of sniffer
    Change-Id: Icbdfa00bee5ba4628515f9a7ea460267eeaa70fe

--- tempest/scenario/test_network_ipv6.py
+++ tempest/scenario/test_network_ipv6.py
@@ -15,6 +15,9 @@
 
 from tempest.scenario import manager
 from tempest import test
+from tempest.common import sniffer
+from tempest.common import radvd
+from tempest.config import CONF
 
 
 class TestNetworkIPv6(manager.NetworkScenarioTest):
@@ -43,3 +46,81 @@ class TestNetworkIPv6(manager.NetworkScenarioTest):
             n_addresses = end.value - start.value + 1
             self.assertEqual(expected=pow(2, 128 - bits)-3,
                              observed=n_addresses)
+
+
+class TestRadvdIPv6(manager.NetworkScenarioTest):
+    _ip_version = 6
+
+    network_resources = {'network': True, 'router': True, 'subnet': True,
+                         'dhcp': True, 'ip_version': 6}
+
+    @test.services('compute', 'network')
+    def test_internal_radvd(self):
+        ex_net = CONF.network.public_network_id
+
+        key_pair = self.create_keypair()
+        sec_group = self._create_security_group_nova()
+        kwargs = {'key_name': key_pair.id,
+                  'security_groups': [sec_group.name]}
+        server = self.create_server(create_kwargs=kwargs)
+        r = sniffer.sniff(what=sniffer.SNIFF_RADVD, count=3)
+        self.assertEqual(expected='icmp6', observed=r['what'])
+        self.assertEqual(expected=3,
+                         observed=r['count'],
+                         message='Wrong number of radvd packets')
+        self.assertEqual(expected=2,
+                         observed=r['advertisement']['count'],
+                         message='Wrong number of RA')
+        self.assertEqual(expected=1,
+                         observed=r['solicitation']['count'],
+                         message='Wrong number of RS')
+
+        if self.run_ssh:
+            fip = self._create_floating_ip(thing=server,
+                                           external_network_id=ex_net)
+            server.add_floating_ip(fip.floating_ip_address)
+            ssh = self.get_remote_client(server_or_ip=fip,
+                                         private_key=key_pair.private_key)
+            capture = ssh.exec_command(sniffer.sniff(what=sniffer.SNIFF_RADVD,
+                                                     count=3,
+                                                     is_remote=True))
+            r = sniffer.sniff_analyzer_radvd(capture=capture)
+            self.assertEqual(expected='icmp6', observed=r['what'])
+            self.assertEqual(expected=3,
+                             observed=r['count'],
+                             message='Wrong number of radvd packets')
+            self.assertEqual(expected=2,
+                             observed=r['advertisement']['count'],
+                             message='Wrong number of RA')
+            self.assertEqual(expected=1,
+                             observed=r['solicitation']['count'],
+                             message='Wrong number of RS')
+
+    @test.services('compute', 'network')
+    def test_two_radvd(self):
+        t_ex = sniffer.sniff_in_thread(what=sniffer.SNIFF_RADVD,
+                                       interface='br-ex',
+                                       count=100,
+                                       timeout=120)
+        t_in = sniffer.sniff_in_thread(what=sniffer.SNIFF_RADVD,
+                                       interface='br-int',
+                                       count=100,
+                                       timeout=120)
+
+        radvd.radvd_start_on(iface='br-ex', prefix='2009::/64')
+
+        key_pair = self.create_keypair()
+        sec_group = self._create_security_group_nova()
+        kwargs = {'key_name': key_pair.id,
+                  'security_groups': [sec_group.name]}
+        self.create_server(create_kwargs=kwargs)
+
+        t_ex.join()
+        t_in.join()
+
+        r_ex = t_ex.result
+        r_in = t_in.result
+
+        self.assertIn('2009::/64', r_ex['advertisement']['prefixes'])
+        self.assertIn(CONF.network.public_network_ipv6_cidr,
+                      r_in['advertisement']['prefixes'])
