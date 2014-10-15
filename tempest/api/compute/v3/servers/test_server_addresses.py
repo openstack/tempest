a698d1777405e4ba22c90db40e49acf87f6bb195
Matt Riedemann <mriedem@us.ibm.com>
2014-10-15 14:33:31 -0700

    Revert "Skip test_list_server_addresses if neutron"
    This reverts commit 3c7f98f0b78444e3cfc411a331d40ceb07629c21.
    
    Since blueprint admin-event-callback-api in Icehouse we shouldn't
    have this issue so enable the test again.
    
    Change-Id: I21aa94068129b856e6a2965ac749c414b8b0cc9e
    Closes-Bug: #1210483

--- tempest/api/compute/v3/servers/test_server_addresses.py
+++ tempest/api/compute/v3/servers/test_server_addresses.py
@@ -14,10 +14,7 @@
 #    under the License.
 
 from tempest.api.compute import base
-from tempest import config
 from tempest import test
-
-CONF = config.CONF
 
 
 class ServerAddressesV3Test(base.BaseV3ComputeTest):
@@ -31,8 +28,6 @@ class ServerAddressesV3Test(base.BaseV3ComputeTest):
 
         resp, cls.server = cls.create_test_server(wait_until='ACTIVE')
 
-    @test.skip_because(bug="1210483",
-                       condition=CONF.service_available.neutron)
     @test.attr(type='smoke')
     def test_list_server_addresses(self):
         # All public and private addresses for
