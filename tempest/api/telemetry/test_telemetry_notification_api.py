e10a3e432951949a0b4ec86ca96a87bbcbacfd3b
gord chung <gord@live.ca>
2015-06-13 00:36:59 -0400

    re-enable glance notification tests
    introduced in Id049b6cb8ab3092c50f35894cf5a6bda7ff04617 and
    disabled by I763212bad849980bea7fdb96384a5fba7889cce7
    
    improvements to sql backend were made in Juno (see bug)
    
    this patch re-enables the tests and makes a modification to the
    expected list of meters (some of original meters are deprecated)
    
    Change-Id: Ia87c120c50d8edf55da3d4b9d748c0680e25a97b
    Related-Bug: #1351627

--- tempest/api/telemetry/test_telemetry_notification_api.py
+++ tempest/api/telemetry/test_telemetry_notification_api.py
@@ -10,7 +10,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from tempest_lib import decorators
 import testtools
 
 from tempest.api.telemetry import base
@@ -46,7 +45,6 @@ class TelemetryNotificationAPITestJSON(base.BaseTelemetryTest):
     @test.services("image")
     @testtools.skipIf(not CONF.image_feature_enabled.api_v1,
                       "Glance api v1 is disabled")
-    @decorators.skip_because(bug='1351627')
     def test_check_glance_v1_notifications(self):
         body = self.create_image(self.image_client)
         self.image_client.update_image(body['id'], data='data')
@@ -63,7 +61,6 @@ class TelemetryNotificationAPITestJSON(base.BaseTelemetryTest):
     @test.services("image")
     @testtools.skipIf(not CONF.image_feature_enabled.api_v2,
                       "Glance api v2 is disabled")
-    @decorators.skip_because(bug='1351627')
     def test_check_glance_v2_notifications(self):
         body = self.create_image(self.image_client_v2)
 
