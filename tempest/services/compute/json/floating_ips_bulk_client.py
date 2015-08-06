0f5b526fce03a521896e5c8d6bcd0cdc67e1e866
ghanshyam <ghanshyam.mann@nectechnologies.in>
2015-08-06 10:50:11 +0900

    Return complete response from floating_ips_bulk_client
    Currently compute floating_ips_bulk_client returns Response by removing
    top key from Response.
    For example-
    return service_client.ResponseBody(resp, body['floating_ips_bulk_create'])
    
    As service clients are in direction to move to Tempest-lib, all
    service clients should return Response without any truncation.
    One good example is Resource pagination links which are lost with current
    way of return value. Resource pagination links are present in parallel
    (not inside) to top key of Response.
    
    This patch makes compute floating_ips_bulk_client to return complete
    Response body.
    
    Change-Id: Ieabe7e207f9086cc7508eb52437b78ab083742cb
    Implements: blueprint method-return-value-and-move-service-clients-to-lib

--- tempest/services/compute/json/floating_ips_bulk_client.py
+++ tempest/services/compute/json/floating_ips_bulk_client.py
@@ -32,18 +32,17 @@ class FloatingIPsBulkClient(service_client.ServiceClient):
         resp, body = self.post('os-floating-ips-bulk', post_body)
         body = json.loads(body)
         self.validate_response(schema.create_floating_ips_bulk, resp, body)
-        return service_client.ResponseBody(resp,
-                                           body['floating_ips_bulk_create'])
+        return service_client.ResponseBody(resp, body)
 
     def list_floating_ips_bulk(self):
-        """Returns a list of all floating IPs bulk."""
+        """Gets all floating IPs in bulk."""
         resp, body = self.get('os-floating-ips-bulk')
         body = json.loads(body)
         self.validate_response(schema.list_floating_ips_bulk, resp, body)
-        return service_client.ResponseBodyList(resp, body['floating_ip_info'])
+        return service_client.ResponseBody(resp, body)
 
     def delete_floating_ips_bulk(self, ip_range):
-        """Deletes the provided floating IPs bulk."""
+        """Deletes the provided floating IPs in bulk."""
         post_body = json.dumps({'ip_range': ip_range})
         resp, body = self.put('os-floating-ips-bulk/delete', post_body)
         body = json.loads(body)
