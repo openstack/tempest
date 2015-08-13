c3e50146376700b6a36f51f62203439c371f7d99
John Warren <jswarren@us.ibm.com>
2015-08-13 13:31:56 +0000

    Full response for v3 RegionClient methods
    Provide the entire response object for all methods of the v3
    RegionClient
    
    partially implements: blueprint method-return-value-and-move-service-clients-to-lib
    
    Change-Id: I01335336fb9980c12340a8c4faaba2576315e485

--- tempest/services/identity/v3/json/region_client.py
+++ tempest/services/identity/v3/json/region_client.py
@@ -37,7 +37,7 @@ class RegionClient(service_client.ServiceClient):
             resp, body = self.post('regions', req_body)
         self.expected_success(201, resp.status)
         body = json.loads(body)
-        return service_client.ResponseBody(resp, body['region'])
+        return service_client.ResponseBody(resp, body)
 
     def update_region(self, region_id, **kwargs):
         """Updates a region."""
@@ -50,7 +50,7 @@ class RegionClient(service_client.ServiceClient):
         resp, body = self.patch('regions/%s' % region_id, post_body)
         self.expected_success(200, resp.status)
         body = json.loads(body)
-        return service_client.ResponseBody(resp, body['region'])
+        return service_client.ResponseBody(resp, body)
 
     def get_region(self, region_id):
         """Get region."""
@@ -58,7 +58,7 @@ class RegionClient(service_client.ServiceClient):
         resp, body = self.get(url)
         self.expected_success(200, resp.status)
         body = json.loads(body)
-        return service_client.ResponseBody(resp, body['region'])
+        return service_client.ResponseBody(resp, body)
 
     def list_regions(self, params=None):
         """List regions."""
@@ -68,7 +68,7 @@ class RegionClient(service_client.ServiceClient):
         resp, body = self.get(url)
         self.expected_success(200, resp.status)
         body = json.loads(body)
-        return service_client.ResponseBodyList(resp, body['regions'])
+        return service_client.ResponseBody(resp, body)
 
     def delete_region(self, region_id):
         """Delete region."""
