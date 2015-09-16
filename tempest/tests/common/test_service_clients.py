8e7a4f820de54d6dd8ce3b805e0fd1a8b0e29ea0
Ken'ichi Ohmichi <oomichi@mxs.nes.nec.co.jp>
2015-09-16 21:26:19 +0900

    Use tempest-lib's agents_client
    Now tempest-lib provides agents_client module as library and the
    interface is stable. So tempest repogitory doesn't need to contain
    the module.
    This patch makes tempest use tempest-lib's agents_client and removes
    the own module for the maintenance.
    
    Change-Id: I610b962de89d71090d70cec7ba5db5bdf0ef75a9

--- tempest/tests/common/test_service_clients.py
+++ tempest/tests/common/test_service_clients.py
@@ -17,7 +17,6 @@ import random
 import six
 
 from tempest.services.baremetal.v1.json import baremetal_client
-from tempest.services.compute.json import agents_client
 from tempest.services.compute.json import aggregates_client
 from tempest.services.compute.json import availability_zone_client
 from tempest.services.compute.json import certificates_client
@@ -109,7 +108,6 @@ class TestServiceClient(base.TestCase):
     def test_service_client_creations_with_specified_args(self, mock_init):
         test_clients = [
             baremetal_client.BaremetalClient,
-            agents_client.AgentsClient,
             aggregates_client.AggregatesClient,
             availability_zone_client.AvailabilityZoneClient,
             certificates_client.CertificatesClient,
