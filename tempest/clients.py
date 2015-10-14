2f6f5f1eb1e0642a7a0f2f39ba5fde4358de4e0a
ghanshyam <ghanshyam.mann@nectechnologies.in>
2015-10-14 14:27:36 +0900

    Use Tempest-lib's compute flavors_client
    Compute flavors_client has been migrated to tempest-lib-
    I3de529b1e0db298530e54fd2c871d40f29614126
    
    Now Tempest-lib provides this client as stable interface so Tempest
    should start using this from lib and remove its own copy.
    Also remove other files for this client like response schema, unit tests.
    
    This commits makes Tempest to use compute flavors_client
    from Tempest-lib.
    
    Change-Id: I8e5b227bb1bf359c4a194f649dad186eb0574a46

--- tempest/clients.py
+++ tempest/clients.py
@@ -27,6 +27,7 @@ from tempest_lib.services.compute.certificates_client import \
 from tempest_lib.services.compute.extensions_client import \
     ExtensionsClient
 from tempest_lib.services.compute.fixed_ips_client import FixedIPsClient
+from tempest_lib.services.compute.flavors_client import FlavorsClient
 from tempest_lib.services.identity.v2.token_client import TokenClient
 from tempest_lib.services.identity.v3.token_client import V3TokenClient
 
@@ -38,7 +39,6 @@ from tempest import manager
 from tempest.services.baremetal.v1.json.baremetal_client import \
     BaremetalClient
 from tempest.services import botoclients
-from tempest.services.compute.json.flavors_client import FlavorsClient
 from tempest.services.compute.json.floating_ip_pools_client import \
     FloatingIPPoolsClient
 from tempest.services.compute.json.floating_ips_bulk_client import \
