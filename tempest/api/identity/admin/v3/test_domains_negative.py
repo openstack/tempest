c849408ffb747fd57b6e4bfe7e181c8fb0917f9b
Wei Liu <wei.liu@easystack.cn>
2015-11-14 03:19:22 +0000

    Add negative test to test_domains_negative
    Add negative test 'delete non existent domain' and
    'create domain with duplicate name'
    to test_domains_negative
    
    Change-Id: I7a964aa588b4dcad8468d9120a2f5789e0780f39
    Partial-Bug: 1513346

--- tempest/api/identity/admin/v3/test_domains_negative.py
+++ tempest/api/identity/admin/v3/test_domains_negative.py
@@ -52,3 +52,21 @@ class DomainsNegativeTestJSON(base.BaseIdentityV3AdminTest):
         d_name = 'a' * 65
         self.assertRaises(lib_exc.BadRequest, self.client.create_domain,
                           d_name)
+
+    @test.attr(type=['negative'])
+    @test.idempotent_id('43781c07-764f-4cf2-a405-953c1916f605')
+    def test_delete_non_existent_domain(self):
+        # Attempt to delete a non existent domain should fail
+        self.assertRaises(lib_exc.NotFound, self.client.delete_domain,
+                          data_utils.rand_uuid_hex())
+
+    @test.attr(type=['negative'])
+    @test.idempotent_id('e6f9e4a2-4f36-4be8-bdbc-4e199ae29427')
+    def test_domain_create_duplicate(self):
+        domain_name = data_utils.rand_name('domain-dup')
+        domain = self.client.create_domain(domain_name)['domain']
+        domain_id = domain['id']
+        self.addCleanup(self.delete_domain, domain_id)
+        # Domain name should be unique
+        self.assertRaises(
+            lib_exc.Conflict, self.client.create_domain, domain_name)
