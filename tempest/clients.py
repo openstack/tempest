6ca351adb6890c58a0a9944ca9df7ed51fcdfae4
ghanshyam <ghanshyam.mann@nectechnologies.in>
2015-11-20 18:35:30 +0900

    Use Tempest-lib's compute networks_client
    Compute networks_client has been migrated to
    tempest-lib- I3d14142e78f72876e4cdc6ce3d3c7cd210b3c670
    
    Now Tempest-lib provides this client as stable interface so Tempest
    should start using this from lib and remove its own copy.
    Also remove its unit tests.
    
    This commit makes Tempest to use compute networks_client
    from Tempest-lib.
    
    Change-Id: Icb48608b64d2cac5b4a41b2b79d55b2c24517e67

--- tempest/clients.py
+++ tempest/clients.py
@@ -40,6 +40,8 @@ from tempest_lib.services.compute.instance_usage_audit_log_client import \
     InstanceUsagesAuditLogClient
 from tempest_lib.services.compute.limits_client import LimitsClient
 from tempest_lib.services.compute.migrations_client import MigrationsClient
+from tempest_lib.services.compute.networks_client import NetworksClient \
+    as ComputeNetworksClient
 from tempest_lib.services.identity.v2.token_client import TokenClient
 from tempest_lib.services.identity.v3.token_client import V3TokenClient
 
@@ -55,8 +57,6 @@ from tempest.services.compute.json.floating_ips_client import \
 from tempest.services.compute.json.interfaces_client import \
     InterfacesClient
 from tempest.services.compute.json.keypairs_client import KeyPairsClient
-from tempest.services.compute.json.networks_client import NetworksClient \
-    as ComputeNetworksClient
 from tempest.services.compute.json.quota_classes_client import \
     QuotaClassesClient
 from tempest.services.compute.json.quotas_client import QuotasClient
