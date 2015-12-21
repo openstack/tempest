9d53dc2d5faf4588900effbcca03ad5857e98f6c
Ghanshyam <ghanshyam.mann@nectechnologies.in>
2015-12-21 11:48:12 +0900

    Use floating_ips_client from tempest-lib
    The files below have been migrated to tempest-lib
    
     * floating_ips_client.py: Ide46dfbd437707c376bb44f86c99a87b739b9ef0
     * floating_ips.py: Iab0f7faca8d23d18061c5b3d13bb8b5e0536025c
     * test_floating_ips_client.py: Ide46dfbd437707c376bb44f86c99a87b739b9ef0
    
    Now Tempest-lib provides those as stable interfaces. So Tempest should
    start using those from lib and remove its own copy.
    
    Change-Id: Ic7c87fa92737a7127fe0194633f8563ad4e61453

--- tempest/tests/common/test_service_clients.py
+++ tempest/tests/common/test_service_clients.py
@@ -17,7 +17,6 @@ import random
 import six
 
 from tempest.services.baremetal.v1.json import baremetal_client
-from tempest.services.compute.json import floating_ips_client
 from tempest.services.compute.json import security_group_rules_client
 from tempest.services.compute.json import server_groups_client
 from tempest.services.compute.json import servers_client
@@ -87,7 +86,6 @@ class TestServiceClient(base.TestCase):
     def test_service_client_creations_with_specified_args(self, mock_init):
         test_clients = [
             baremetal_client.BaremetalClient,
-            floating_ips_client.FloatingIPsClient,
             security_group_rules_client.SecurityGroupRulesClient,
             server_groups_client.ServerGroupsClient,
             servers_client.ServersClient,
