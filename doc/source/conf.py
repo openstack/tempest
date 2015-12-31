d11d5ee09de8d2d15c18519cdc5fa3aedd985ac2
Harshada Mangesh Kakad <harshadak@metsi.co.uk>
2015-12-31 01:38:46 -0800

    Fixing the deprecated library function.
    os.popen() is deprecated since version 2.6. Resolved with use of
    subprocess module.
    
    Change-Id: I0751dbda085922d5e390cb7a19a9e832d399e20b
    Partial-Bug: #1529836

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -127,8 +127,11 @@ html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-git_cmd = "git log --pretty=format:'%ad, commit %h' --date=local -n1"
-html_last_updated_fmt = os.popen(git_cmd).read()
+git_cmd = ["git", "log", "--pretty=format:'%ad, commit %h'", "--date=local",
+   "-n1"]
+html_last_updated_fmt = subprocess.Popen(git_cmd,
+                                         stdout=subprocess.PIPE).\
+                                         communicate()[0]
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
