be67e2db50eb8595fcc7894da80fd2e7e6ff2541
zhufl <zhu.fanglei@zte.com.cn>
2016-02-02 16:18:22 +0800

    skip overlimit tests when default quota set is -1
    testcase <test_resize_server_using_overlimit_ram> is intended to create
    flavor using overlimit ram, but if the tenant's default ram quota set
    is -1, we will get "ERROR (BadRequest): Quota limit 0 for cores must be
    greater than or equal to already used and reserved 1. (HTTP 400)" when
    creating flavor with ram=0(-1 + 1 = 0).
    
    so testcase <test_resize_server_using_overlimit_ram> should be skipped
    if default quota set is -1
    
    <test_resize_server_using_overlimit_vcpus> is the same.
    
    Change-Id: Ia823b4b0170119cd8f17c72e6eabae0a4a042c9e
    Closes-Bug: #1540780

--- tempest/api/compute/admin/test_servers_negative.py
+++ tempest/api/compute/admin/test_servers_negative.py
@@ -68,7 +68,11 @@ class ServersAdminNegativeTestJSON(base.BaseV2ComputeAdminTest):
         flavor_id = self._get_unused_flavor_id()
         quota_set = (self.quotas_client.show_default_quota_set(self.tenant_id)
                      ['quota_set'])
-        ram = int(quota_set['ram']) + 1
+        ram = int(quota_set['ram'])
+        if ram == -1:
+            raise self.skipException("default ram quota set is -1,"
+                                     " cannot test overlimit")
+        ram += 1
         vcpus = 8
         disk = 10
         flavor_ref = self.flavors_client.create_flavor(name=flavor_name,
@@ -93,7 +97,11 @@ class ServersAdminNegativeTestJSON(base.BaseV2ComputeAdminTest):
         ram = 512
         quota_set = (self.quotas_client.show_default_quota_set(self.tenant_id)
                      ['quota_set'])
-        vcpus = int(quota_set['cores']) + 1
+        vcpus = int(quota_set['cores'])
+        if vcpus == -1:
+            raise self.skipException("default cores quota set is -1,"
+                                     " cannot test overlimit")
+        vcpus += 1
         disk = 10
         flavor_ref = self.flavors_client.create_flavor(name=flavor_name,
                                                        ram=ram, vcpus=vcpus,
