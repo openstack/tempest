deabf435dc0c70f46315db6bf98409b801188c3d
Pablo Sanchez <psanchez@redhat.com>
2016-06-23 09:58:42 +0200

    Fixed typo in in data_utils.py
    Change-Id: I30180aaf111b0e5013ef4436cf5dc60753561ddd

--- tempest/lib/common/utils/data_utils.py
+++ tempest/lib/common/utils/data_utils.py
@@ -154,7 +154,7 @@ def arbitrary_string(size=4, base_text=None):
     This generates a string with an arbitrary number of characters, generated
     by looping the base_text string. If the size is smaller than the size of
     base_text, returning string is shrinked to the size.
-    :param int size: a returning charactors size
+    :param int size: a returning characters size
     :param str base_text: a string you want to repeat
     :return: size string
     :rtype: string
