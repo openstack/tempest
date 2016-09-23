d004afa590cc14b25f75d9c3c320f3b81d48c501
Li Wei <wei.li@easystack.cn>
2016-09-23 17:20:06 +0800

    Add test list namespace function
    Both test_images.py and test_images_member.py have test list funtion,
    we can also add a function in test_images_metadefs_namespaces.py for
    testing namespaces list.
    
    Another reason is we have done create, get detail, update and delete
    namespace test. But did not test the list.
    
    Related link:
    http://developer.openstack.org/api-ref/image/v2/metadefs-index.html
    
    Change-Id: I4caaccec9dea29f6a368f013a3edaad893fafada

--- tempest/lib/services/image/v2/namespaces_client.py
+++ tempest/lib/services/image/v2/namespaces_client.py
@@ -34,6 +34,18 @@ class NamespacesClient(rest_client.RestClient):
         body = json.loads(body)
         return rest_client.ResponseBody(resp, body)
 
+    def list_namespaces(self):
+        """List namespaces
+
+        Available params: see http://developer.openstack.org/
+                              api-ref/image/v2/metadefs-index.html#list-namespaces
+        """
+        url = 'metadefs/namespaces'
+        resp, body = self.get(url)
+        self.expected_success(200, resp.status)
+        body = json.loads(body)
+        return rest_client.ResponseBody(resp, body)
+
     def show_namespace(self, namespace):
         """Show namespace details.
 
