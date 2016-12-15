0df8076ed100885a122f1ce83b1ed543b939e0dd
zhufl <zhu.fanglei@zte.com.cn>
2016-12-15 10:15:40 +0800

    Use ephemeral=0 to create flavor without ephemeral disk
    When creating flavor, whether <"ephemeral": 0> is included
    in request body will get same results <"OS-FLV-EXT-DATA:ephemeral": 0>,
    so we needn't use different code to create flavor with or without
    ephemeral disk.
    Besides, it seems not necessary to use different flavor names for
    flavor with or without ephemeral disk, the info can easily be got
    from flavor info. So the code can become more simple.
    
    Change-Id: If0a63ff401f34caebff00b7925b08fe02b3eb5d3

--- tempest/api/compute/servers/test_create_server.py
+++ tempest/api/compute/servers/test_create_server.py
@@ -253,24 +253,18 @@ class ServersWithSpecificFlavorTestJSON(base.BaseV2ComputeAdminTest):
             self.flavor_ref)['flavor']
 
         def create_flavor_with_ephemeral(ephem_disk):
-            flavor_with_eph_disk_id = data_utils.rand_int_id(start=1000)
+            flavor_id = data_utils.rand_int_id(start=1000)
+            name = 'flavor_with_ephemeral_%s' % ephem_disk
+            flavor_name = data_utils.rand_name(name)
 
             ram = flavor_base['ram']
             vcpus = flavor_base['vcpus']
             disk = flavor_base['disk']
 
-            if ephem_disk > 0:
-                # Create a flavor with ephemeral disk
-                flavor_name = data_utils.rand_name('eph_flavor')
-                flavor = self.flavor_client.create_flavor(
-                    name=flavor_name, ram=ram, vcpus=vcpus, disk=disk,
-                    id=flavor_with_eph_disk_id, ephemeral=ephem_disk)['flavor']
-            else:
-                # Create a flavor without ephemeral disk
-                flavor_name = data_utils.rand_name('no_eph_flavor')
-                flavor = self.flavor_client.create_flavor(
-                    name=flavor_name, ram=ram, vcpus=vcpus, disk=disk,
-                    id=flavor_with_eph_disk_id)['flavor']
+            # Create a flavor with ephemeral disk
+            flavor = self.flavor_client.create_flavor(
+                name=flavor_name, ram=ram, vcpus=vcpus, disk=disk,
+                id=flavor_id, ephemeral=ephem_disk)['flavor']
             self.addCleanup(flavor_clean_up, flavor['id'])
 
             return flavor['id']
