b2f309002e729adce4e210d3816fca95f67f6de8
zhufl <zhu.fanglei@zte.com.cn>
2017-03-08 11:29:08 +0800

    Correct API reference link in volume client
    Some API reference links in volume client are invalid now, this
    is to change to the current link.
    
    Change-Id: Ib06bbd1f7523659ad5624a16d099ee902b29e4a7

--- tempest/lib/services/volume/v2/backups_client.py
+++ tempest/lib/services/volume/v2/backups_client.py
@@ -28,7 +28,7 @@ class BackupsClient(rest_client.RestClient):
 
         For a full list of available parameters, please refer to the official
         API reference:
-        http://developer.openstack.org/api-ref-blockstorage-v2.html#createBackup
+        https://developer.openstack.org/api-ref/block-storage/v2/index.html#create-backup
         """
         post_body = json.dumps({'backup': kwargs})
         resp, body = self.post('backups', post_body)
@@ -41,7 +41,7 @@ class BackupsClient(rest_client.RestClient):
 
         For a full list of available parameters, please refer to the official
         API reference:
-        http://developer.openstack.org/api-ref-blockstorage-v2.html#restoreBackup
+        https://developer.openstack.org/api-ref/block-storage/v2/index.html#restore-backup
         """
         post_body = json.dumps({'restore': kwargs})
         resp, body = self.post('backups/%s/restore' % (backup_id), post_body)
