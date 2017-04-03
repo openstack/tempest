6f52fc599a430723815b4bbc84a82ba10c958f75
Hai Shi <shihai1992@gmail.com>
2017-04-03 21:17:37 +0800

    [DOC] Fix details of tox
    We need put a space between a param's name and a param's value in tox
    and it would be more clear.
    eg: from `tox -epy35` to `tox -e py35`.
    
    Change-Id: I86750f42669bb02dcfac2b62e294d557bf44bd4c

--- tempest/lib/cmd/check_uuid.py
+++ tempest/lib/cmd/check_uuid.py
@@ -355,7 +355,7 @@ def run():
     if errors:
         sys.exit("@decorators.idempotent_id existence and uniqueness checks "
                  "failed\n"
-                 "Run 'tox -v -euuidgen' to automatically fix tests with\n"
+                 "Run 'tox -v -e uuidgen' to automatically fix tests with\n"
                  "missing @decorators.idempotent_id decorators.")
 
 if __name__ == '__main__':
