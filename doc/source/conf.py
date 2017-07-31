15b3842665e53994adb7683714d3815b68ae45bb
junboli <junbo85.li@gmail.com>
2017-07-31 15:32:19 +0800

    TrivialFix: Remove the unused import code
    In the file doc/source/conf.py, some unused import code left, this change is to
    clean them up.
    
    Change-Id: I22ad2c3f6f37e0ca80c2a0d0943492572899929e

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,10 +22,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
 import os
 import subprocess
-import warnings
 
 import openstackdocstheme
 
