eb83b0f36c48e3dbac004adf3204188b7b29d549
zhufl <zhu.fanglei@zte.com.cn>
2017-08-01 13:29:59 +0800

    Fix broken api links in lib/services
    This is to fix broken api links in lib/services.
    
    Change-Id: If6f389bd5a87fec4a5be4e1915109cb438cbf9e3

--- tempest/lib/services/identity/v3/catalog_client.py
+++ tempest/lib/services/identity/v3/catalog_client.py
@@ -11,8 +11,7 @@
 #    under the License.
 
 """
-https://developer.openstack.org/api-ref/identity/v3/index.html#\
-get-service-catalog
+https://developer.openstack.org/api-ref/identity/v3/index.html#get-service-catalog
 """
 
 from oslo_serialization import jsonutils as json
