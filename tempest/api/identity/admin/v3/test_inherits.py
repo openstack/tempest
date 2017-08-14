f6bae31bb407d831872d0e5cc6d58819bfda5f7c
zhufl <zhu.fanglei@zte.com.cn>
2017-08-14 13:37:53 +0800

    Remove unnecessary class Inheritance
    This is to remove the unnecessary class inheritance
    to make code more readable and easy to debug.
    
    Change-Id: Id98ad9df2eada05d3e14d3c523bc7876ef6ecbbe

--- tempest/api/identity/admin/v3/test_inherits.py
+++ tempest/api/identity/admin/v3/test_inherits.py
@@ -16,17 +16,17 @@ from tempest.lib import decorators
 from tempest import test
 
 
-class BaseInheritsV3Test(base.BaseIdentityV3AdminTest):
+class InheritsV3TestJSON(base.BaseIdentityV3AdminTest):
 
     @classmethod
     def skip_checks(cls):
-        super(BaseInheritsV3Test, cls).skip_checks()
+        super(InheritsV3TestJSON, cls).skip_checks()
         if not test.is_extension_enabled('OS-INHERIT', 'identity'):
             raise cls.skipException("Inherits aren't enabled")
 
     @classmethod
     def resource_setup(cls):
-        super(BaseInheritsV3Test, cls).resource_setup()
+        super(InheritsV3TestJSON, cls).resource_setup()
         u_name = data_utils.rand_name('user-')
         u_desc = '%s description' % u_name
         u_email = '%s@testmail.tm' % u_name
@@ -51,14 +51,11 @@ class BaseInheritsV3Test(base.BaseIdentityV3AdminTest):
         cls.projects_client.delete_project(cls.project['id'])
         cls.domains_client.update_domain(cls.domain['id'], enabled=False)
         cls.domains_client.delete_domain(cls.domain['id'])
-        super(BaseInheritsV3Test, cls).resource_cleanup()
+        super(InheritsV3TestJSON, cls).resource_cleanup()
 
     def _list_assertions(self, body, fetched_role_ids, role_id):
         self.assertEqual(len(body), 1)
         self.assertIn(role_id, fetched_role_ids)
-
-
-class InheritsV3TestJSON(BaseInheritsV3Test):
 
     @decorators.idempotent_id('4e6f0366-97c8-423c-b2be-41eae6ac91c8')
     def test_inherit_assign_list_check_revoke_roles_on_domains_user(self):
