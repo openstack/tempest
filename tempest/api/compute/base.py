openstack/tempest

commit 1dc2e231d1aa423a6b25eef066ab0fe66b9ca324

Only choose available compute node as migration dest
In live_migrate_server, destination host can be specified,
but we should only choose available compute node as the
destination host, otherwise the testcase will fail unexpectedly.

Change-Id: I0a293161efa346a5cc6a57ea794641364b4b2d25

tempest/api/compute/base.py
@@ -495,12 +495,10 @@ class BaseV2ComputeAdminTest(BaseV2ComputeTest):
     def get_host_other_than(self, server_id):
         source_host = self.get_host_for_server(server_id)
 
-        list_hosts_resp = self.os_admin.hosts_client.list_hosts()['hosts']
-        hosts = [
-            host_record['host_name']
-            for host_record in list_hosts_resp
-            if host_record['service'] == 'compute'
-        ]
+        hypers = self.os_admin.hypervisor_client.list_hypervisors(
+            )['hypervisors']
+        hosts = [hyper['hypervisor_hostname'] for hyper in hypers
+                 if hyper['state'] == 'up' and hyper['status'] == 'enabled']
 
         for target_host in hosts:
             if source_host != target_host:

commit 50325e239ad0119c6e0cfd90b1c7b1ef7ec78525

Don't use hypervisor_hostname as compute hostname
Several tempest tests are failing when using the VMware virt driver
where compute hostname != hypervisor_hostname. Those are different
things and cannot be used interchangeably. This patch fixes a
regression introduced with commit 1dc2e231 where the hosts_client was
replaced with hypervisor_client in order to select hosts which are both
up and enabled.

This patch changes the implementation of get_host_other_than() to use
services_client which still allows to select nova computes which are up
and enabled but also gives us the compute hostname.

Change-Id: Ie6df1192e1ca83977dccc8e6b9331dfe874c8aba
Closes-Bug: #1759801

tempest/api/compute/base.py
@@ -532,10 +532,10 @@ class BaseV2ComputeAdminTest(BaseV2ComputeTest):
     def get_host_other_than(self, server_id):
         source_host = self.get_host_for_server(server_id)
 
-        hypers = self.os_admin.hypervisor_client.list_hypervisors(
-            )['hypervisors']
-        hosts = [hyper['hypervisor_hostname'] for hyper in hypers
-                 if hyper['state'] == 'up' and hyper['status'] == 'enabled']
+        svcs = self.os_admin.services_client.list_services(
+            binary='nova-compute')['services']
+        hosts = [svc['host'] for svc in svcs
+                 if svc['state'] == 'up' and svc['status'] == 'enabled']
 
         for target_host in hosts:
             if source_host != target_host: