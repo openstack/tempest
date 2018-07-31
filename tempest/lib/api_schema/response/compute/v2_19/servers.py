e612431541bed94110f22294300feb462a7fd774
ghanshyam <gmann@ghanshyammann.com>
2018-07-31 08:15:59 +0000

    Add server schema for microversion 2.19
    This commit adds servers schema for microversion 2.19
    Without those schema in this file, get_schema() method
    will throw error about particular schema not found in this file.
    
    Change-Id: Ia4a1e27044ce210d86b386a6208989d4f0e9c1c6
    partially-implements: blueprint full-schema-for-all-microversions

--- tempest/lib/api_schema/response/compute/v2_19/servers.py
+++ tempest/lib/api_schema/response/compute/v2_19/servers.py
@@ -16,9 +16,6 @@ import copy
 
 from tempest.lib.api_schema.response.compute.v2_16 import servers \
     as serversv216
-from tempest.lib.api_schema.response.compute.v2_9 import servers as serversv29
-
-list_servers = copy.deepcopy(serversv216.list_servers)
 
 get_server = copy.deepcopy(serversv216.get_server)
 get_server['response_body']['properties']['server'][
@@ -32,21 +29,29 @@ list_servers_detail['response_body']['properties']['servers']['items'][
 list_servers_detail['response_body']['properties']['servers']['items'][
     'required'].append('description')
 
-update_server = copy.deepcopy(serversv29.update_server)
+update_server = copy.deepcopy(serversv216.update_server)
 update_server['response_body']['properties']['server'][
     'properties'].update({'description': {'type': ['string', 'null']}})
 update_server['response_body']['properties']['server'][
     'required'].append('description')
 
-rebuild_server = copy.deepcopy(serversv29.rebuild_server)
+rebuild_server = copy.deepcopy(serversv216.rebuild_server)
 rebuild_server['response_body']['properties']['server'][
     'properties'].update({'description': {'type': ['string', 'null']}})
 rebuild_server['response_body']['properties']['server'][
     'required'].append('description')
 
 rebuild_server_with_admin_pass = copy.deepcopy(
-    serversv29.rebuild_server_with_admin_pass)
+    serversv216.rebuild_server_with_admin_pass)
 rebuild_server_with_admin_pass['response_body']['properties']['server'][
     'properties'].update({'description': {'type': ['string', 'null']}})
 rebuild_server_with_admin_pass['response_body']['properties']['server'][
     'required'].append('description')
+
+# NOTE(gmann): Below are the unchanged schema in this microversion. We need
+# to keep this schema in this file to have the generic way to select the
+# right schema based on self.schema_versions_info mapping in service client.
+# ****** Schemas unchanged since microversion 2.16 ******
+list_servers = copy.deepcopy(serversv216.list_servers)
+show_server_diagnostics = copy.deepcopy(serversv216.show_server_diagnostics)
+get_remote_consoles = copy.deepcopy(serversv216.get_remote_consoles)
