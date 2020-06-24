217eebd0bfb5f8ed4ec1ad75c07bb419534989f6
melissaml <ma.lei@99cloud.net>
2020-06-24 10:13:45 +0800

    Fix pygments style
    New theme of docs respects pygments_style.
    
    more info: http://lists.openstack.org/pipermail/openstack-discuss/2020-May/014971.html
    
    Change-Id: I4fbc5fc613e6eb64997935422697477db3ebcc91

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -112,7 +112,7 @@ add_module_names = False
 show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'native'
 
 # A list of ignored prefixes for module index sorting.
 modindex_common_prefix = ['tempest.']
