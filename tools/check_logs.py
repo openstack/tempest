fe0e0a679de820fd823415d68e2d57147c51335c
songwenping <songwenping@inspur.com>
2022-04-19 11:13:55 +0800

    Fix: change values to items as keys and values are all used
    Change-Id: Iaecb5995f2525aa7acc17b769646c2b878a3c58b

--- tools/check_logs.py
+++ tools/check_logs.py
@@ -136,7 +136,7 @@ def main(opts):
     with open(ALLOW_LIST_FILE) as stream:
         loaded = yaml.safe_load(stream)
         if loaded:
-            for (name, l) in loaded.values():
+            for (name, l) in loaded.items():
                 for w in l:
                     assert 'module' in w, 'no module in %s' % name
                     assert 'message' in w, 'no message in %s' % name
