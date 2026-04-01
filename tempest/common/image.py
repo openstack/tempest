d1a4ee4ee31f8edf1ac23718b2d6f33ec4329eb6
Ghanshyam Maan <gmaan.os14@gmail.com>
2026-04-01 20:42:38 +0000

    Remove Glance v1 APIs specific code
    Below change removes the Glance v1 APIs tests
    specific code from common.image.py file.
    
    Glance V1 APIs test has been removed in
    - https://review.opendev.org/c/openstack/tempest/+/890592
    
    Change-Id: I0294cc29479ac16c1cb3d1a2d9abc7ea0148f02b
    Signed-off-by: Ghanshyam Maan <gmaan.os14@gmail.com>

--- tempest/common/image.py
+++ tempest/common/image.py
@@ -13,60 +13,10 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import copy
 from http import server
 import random
 import threading
 import time
-
-
-def get_image_meta_from_headers(resp):
-    meta = {'properties': {}}
-    for key in resp.response:
-        value = resp.response[key]
-        if key.startswith('x-image-meta-property-'):
-            _key = key[22:]
-            meta['properties'][_key] = value
-        elif key.startswith('x-image-meta-'):
-            _key = key[13:]
-            meta[_key] = value
-
-    for key in ['is_public', 'protected', 'deleted']:
-        if key in meta:
-            meta[key] = meta[key].strip().lower() in ('t', 'true', 'yes', '1')
-
-    for key in ['size', 'min_ram', 'min_disk']:
-        if key in meta:
-            try:
-                meta[key] = int(meta[key])
-            except ValueError:
-                pass
-    return meta
-
-
-def image_meta_to_headers(**metadata):
-    headers = {}
-    fields_copy = copy.deepcopy(metadata)
-
-    copy_from = fields_copy.pop('copy_from', None)
-    purge = fields_copy.pop('purge_props', None)
-
-    if purge is not None:
-        headers['x-glance-registry-purge-props'] = purge
-
-    if copy_from is not None:
-        headers['x-glance-api-copy-from'] = copy_from
-
-    for key, value in fields_copy.pop('properties', {}).items():
-        headers['x-image-meta-property-%s' % key] = str(value)
-
-    for key, value in fields_copy.pop('api', {}).items():
-        headers['x-glance-api-property-%s' % key] = str(value)
-
-    for key, value in fields_copy.items():
-        headers['x-image-meta-%s' % key] = str(value)
-
-    return headers
 
 
 class RandomDataHandler(server.BaseHTTPRequestHandler):
